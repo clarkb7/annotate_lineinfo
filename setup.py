ef0cbe4be5737ccce8d70248f8b4f8ab6b7b6a02
clarkb7 <brandenjclark+github@gmail.com>
2019-03-24 01:01:46 -0400

    Add long description

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open("README.md", "r") as f:
+    long_desc = f.read()
+
 setup(
     name='annotate_lineinfo',
     version='0.1',
     description='Annotate IDA with source and line number information from a PDB',
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
     url='https://github.com/clarkb7/annotate_lineinfo',
     license='MIT',
 
