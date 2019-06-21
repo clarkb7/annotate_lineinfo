201f648d092f713b7f85b0ccaf08293eb7e71b80
clarkb7 <brandenjclark+github@gmail.com>
2019-06-20 23:52:43 -0400

    Add IDA plugin install parameter to setup.py

--- README.md
+++ README.md
@@ -15,10 +15,13 @@ annotate_lineinfo.ida_annotate_lineinfo()
 
 ### Plugin
 To install
-* Place [annotate_lineinfo_plugin.py](annotate_lineinfo_plugin.py) in the `plugins` directory of your IDA installation.
+* Option 1) Run `python setup.py install --install-ida-plugin=PATH` to install [annotate_lineinfo_plugin.py](annotate_lineinfo_plugin.py) to `PATH\plugins`
+  * If `PATH` is not specified, `%IDAUSR%` will be tried first
+  * If `%IDAUSR%` does not exist, it defaults to `%APPDATA%\Hex-Rays\IDA Pro`
+* Option 2) Manually place [annotate_lineinfo_plugin.py](annotate_lineinfo_plugin.py) in the `plugins` directory of your IDA installation.
 
 Annotate entire file
-* Use shortcut key `Alt-Shift-A` or run from "Edit->Annotate lineinfo" menu.
+* Use shortcut key `Alt-Shift-A` or run from `Edit->Annotate lineinfo` menu.
 
 Disassembly view popup menu
 * Right click inside a function, select annotate
@@ -27,10 +30,15 @@ Disassembly view popup menu
 Functions view popup menu
 * Select one or more functions, right click, select annotate
 
-Places searched for PDB file:
-* `_NT_SYMNOL_PATH` if set
+Each of the above actions has a corresponding `remove annotations` action.
+
+On load, annotate_lineinfo attempts to locate the PDB in the following locations:
+* `_NT_SYMBOL_PATH` if set
 * IDA's default PDB download directory `%TEMP%\ida`
 * MSDIA defaults - Path in debug directory of executable, same path as executable
+
+You may specify the PDB path manually, or request another auto-locate attempt (e.g. after IDA downloads the PDB),
+from the `Edit->Annotate lineinfo` menu.
 
 ## Caveats
 Only runs on Windows. This script makes use of the COM API provided by msdia[ver].dll to parse the PDB.
--- setup.py
+++ setup.py
@@ -1,4 +1,46 @@
+import os
+import shutil
+
 from setuptools import setup
+
+import setuptools.command.install as install
+from distutils.errors import DistutilsArgError
+from distutils import log
+
+class CustomInstallCommand(install.install):
+    user_options = install.install.user_options + [
+        ('install-ida-plugin=', None, 'Install plugin in this IDA installation directory, empty arg defaults to IDAUSR directory'),
+    ]
+    def initialize_options(self):
+        self.install_ida_plugin = None
+        install.install.initialize_options(self)
+    def run(self):
+        path = None
+        if self.install_ida_plugin is not None:
+            base = None
+            if self.install_ida_plugin == '':
+                # Try IDAUSR first
+                base = os.getenv("IDAUSR")
+                if base is None:
+                    # Use default from IDA docs
+                    base = os.getenv("APPDATA")
+                    if base is not None:
+                        base = os.path.join(base, 'Hex-Rays', 'IDA Pro')
+            else:
+                base = self.install_ida_plugin
+            if base and os.path.exists(base):
+                path = os.path.join(base, 'plugins')
+            if path is None:
+                raise DistutilsArgError("Unable to locate IDA installation, pleasy specify")
+
+        install.install.do_egg_install(self)
+        # Install plugin
+        if path is not None:
+            log.info("Installing IDA plugin to: {}".format(path))
+            if not os.path.exists(path):
+                os.makedirs(path)
+            plugin_file = 'annotate_lineinfo_plugin.py'
+            shutil.copyfile(plugin_file, os.path.join(path, plugin_file))
 
 with open("README.md", "r") as f:
     long_desc = f.read()
@@ -30,4 +72,8 @@ setup(
     packages=['annotate_lineinfo'],
     py_modules=['annotate_lineinfo_plugin'],
     install_requires=['comtypes'],
+
+    cmdclass={
+        'install': CustomInstallCommand,
+    },
 )
